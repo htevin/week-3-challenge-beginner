96c5c0db71c2e74923b26fa08e1f3352380f909f
htevin <tevinhamilton@yahoo.com>
2022-09-05 11:34:27 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,118 @@
+// function addition(num1, num2) {
+//    return num1 + num2
+// }
+
+// console.log(addition(3, 5));
+// console.log(addition(-3, -6));
+// console.log(addition(7, 3));
+
+// function hoursIntoSeconds (hour){
+   // let minutes = hour * 60;
+   // let seconds = minutes * 60;
+    //return seconds;
+// }
+
+// console.log(hoursIntoSeconds(2))
+
+//function perimeter (lenght, width) {
+//return 2 * lenght + 2 * width;
+//}
+
+//console.log(perimeter(6, 7))
+
+//function calcTriangleArea (base, height){
+ //   return 0.5 * base * height
+//}
+
+//console.log(calcTriangleArea(20,20))
+
+//function appendFrontend (string){
+//    return string + 'Frontend'
+//}
+
+//console.log(appendFrontend('Apple'))
+
+//function sumGreaterThan100 (num1, num2){
+  //  if (num1 + num2 > 100){ 
+  //      return true;
+  //  }
+  //  else return false
+   // }
+ 
+   // console.log(sumGreaterThan100(100, 10))
+
+    //shorter version
+
+   // function sumGreaterThan100 (num1, num2){
+    //    return num1 + num2 > 100}
+     
+     //   console.log(sumGreaterThan100(100, 10))
+
+     //function lessThanOrEqualToZero (num1){
+       // if (num1 <= 0){
+         //   return true
+        //}
+        //else return false
+     //}
+
+     //console.log(lessThanOrEqualToZero(5))
+
+     //function lessThanOrEqualToZero (num1){
+      //  return num1 <= 0}
+
+      //  console.log(lessThanOrEqualToZero(-5))
+
+      //function oppositeBoolean(bool){
+       // if (bool === true){
+        //    return false
+        //}
+        //return true}
+
+        //console.log(oppositeBoolean(true))
+        //function oppositeBoolean(bool){
+        //return !bool;}
+    
+          //  console.log(oppositeBoolean(false))
+         
+          //function isEven(num1) {
+           // if (num1 % 2 === 0){
+           //     return "1"
+           // }
+           // return "-1"
+          //}
+
+          //console.log(isEven(25))
+
+  //        function booleanInteger (num1) {
+   //         if (num1 % 2 === 0){
+   //           return 1
+   //         }
+   //         return -1
+   //       }
+    
+    //      console.log(booleanInteger(15))
+
+
+    // short version of above
+
+  //  function booleanInteger (num1) {
+  //    return num1 % 2 === 0 ? 1 : -1
+  //  }
+//
+  //  console.log(booleanInteger(12))
+
+  //function isLoggedInAndSubscribed (LOGGED_IN, SUBSCRIBED){
+   // if (LOGGED_IN === `LOGGED_IN` && SUBSCRIBED === `SUBSCRIBED`){
+   //   return true
+   // }
+   // return false
+  //}
+
+ // console.log(isLoggedInAndSubscribed(`LOGGED_IN`, `NOTSUBSCRIBED`))
+
+
+ function booleanInteger(num){
+  return num % 2 }
+  
+
+ console.log(booleanInteger(4))
